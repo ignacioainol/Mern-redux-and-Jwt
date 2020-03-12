a5e9a752f9754b098ca1c14aa552c88590f93c3b
ignacioainol <iggnaxios@gmail.com>
2020-03-12 16:21:13 -0300

    added client react and add reactstrap for styling

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import AppNavbar from './components/AppNavbar';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <AppNavbar/>
+      <h1>Hello</h1>
+    </div>
+  );
+}
+
+export default App;
